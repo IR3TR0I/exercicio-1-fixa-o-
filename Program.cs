8819583b7a8caed910ea0de7af123eb6ef2722fc
Ruan <ruangustavo50711@gmail.com>
2020-11-23 16:37:39 -0300

    exercicio 2 finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercicio_1_fixação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("qual o ano em que você nasceu :");
+            int ano = int.Parse(Console.ReadLine());
+                int idade = (2020 - ano);
+
+            if(idade >=18){
+                Console.WriteLine("Parabens Vote Consciente!!");
+            }
+            else {
+                Console.WriteLine("Sinto muito, Você ainda não tem idade para votar!");
+            }
+
+        }
+    }
+}
